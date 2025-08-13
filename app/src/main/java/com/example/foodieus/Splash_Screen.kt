0e3301766361af18be8b9cd9e520f21a15371d6f
Tushar Sharma <tusharsharma2822@gmail.com>
2025-08-13 13:26:45 +0530

    Android App development Tutorial

---
+++ app/src/main/java/com/example/foodieus/Splash_Screen.kt
@@ -0,0 +1,29 @@
+package com.example.foodieus
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.content.Intent
+import android.os.Handler
+
+@Suppress("DEPRECATION")
+class Splash_Screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash_screen)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        Handler().postDelayed({
+            val intent = Intent(this, StartActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
